2978889995509b701ab6f7346513d681c44212b5
TheRevBv <joshmorin417@gmail.com>
2022-07-02 01:28:31 -0500

    Initial Commirt

---
+++ josh-market/src/main/java/com/therev/market/JoshMarketApplication.java
@@ -0,0 +1,13 @@
+package com.therev.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoshMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoshMarketApplication.class, args);
+	}
+
+}
